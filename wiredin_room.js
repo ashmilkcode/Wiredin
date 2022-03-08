3ac80b5021486fd71878fd6cf362a729abec039c
Lakshmi <84883977+bluearrowheadgirl@users.noreply.github.com>
2022-03-08 17:46:28 -0500

    Add files via upload

--- wiredin_room.js
+++ wiredin_room.js
@@ -1,43 +1,60 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAqf9_Wm8-WXW9-24XxMr-x2-eiYTtyMsU",
-    authDomain: "kwitter-3d0be.firebaseapp.com",
-    projectId: "kwitter-3d0be",
-    storageBucket: "kwitter-3d0be.appspot.com",
-    messagingSenderId: "87485751371",
-    appId: "1:87485751371:web:d26e93254bae37d7b32dce"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  
+  apiKey: "AIzaSyAqf9_Wm8-WXW9-24XxMr-x2-eiYTtyMsU",
+  authDomain: "kwitter-3d0be.firebaseapp.com",
+  projectId: "kwitter-3d0be",
+  storageBucket: "kwitter-3d0be.appspot.com",
+  messagingSenderId: "87485751371",
+  appId: "1:87485751371:web:d26e93254bae37d7b32dce"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 //ADD YOUR FIREBASE LINKS HERE
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-  Room_names = childKey;
- //Start code
- console.log("Room Name - " + Room_names);
- row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
- document.getElementById("output").innerHTML += row;
- //End code
- });});}
+   Room_names = childKey;
+  //Start code
+  console.log("Room Name - " + Room_names);
+  row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+  document.getElementById("output").innerHTML += row;
+  //End code
+  });});}
 getData();
 
 function addRoom()
 {
- room_name = document.getElementById("room_name").value;
+  room_name = document.getElementById("room_name").value;
 
- firebase.database().ref("/").child(room_name).update({
-   purpose : "adding room name"
- });
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
 
-   localStorage.setItem("room_name", room_name);
+    localStorage.setItem("room_name", room_name);
 
-   window.location = "wiredin_page.html";
+    window.location = "wiredin_page.html";
 }
 
 function redirectToRoomName(name)
 {
- console.log(name);
- localStorage.setItem("room_name", name);
-   window.location = "wiredin_page.html";
+  console.log(name);
+  localStorage.setItem("room_name", name);
+    window.location = "wiredin_page.html";
+}
+
+function logout() {
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+window.location = "wiredin.html";
+}
+function send()
+{
+msg = document.getElementById("msg").value;
+firebase.database().ref(room_name).push({
+name:user_name,
+message:msg,
+like:0
+});
+
+document.getElementById("msg").value = "";
 }
